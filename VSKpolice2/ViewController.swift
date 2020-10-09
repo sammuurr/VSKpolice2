08518b53b76b423b119a3cbee61a73f3691f3ab5
Geniy <geniy@gmail.com>
2020-10-09 08:49:51 +0300

    Initial Commit

---
+++ VSKpolice2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VSKpolice2
+//
+//  Created by ADMIMN on 09.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
